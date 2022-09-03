db86466751a3bf1552cac34dcbe0e9336e1b2a02
Rob Cobb <rwcobbjr@gmail.com>
2022-09-02 23:44:51 -0600

    add solution

--- main.py
+++ main.py
@@ -34,14 +34,26 @@ roster = [
 
 # Write your solution below
 
+# Print the current roster
 print("\nThe current Real Madrid roster:\n")
-# Print the current roster using a for loop
+for player in roster:
+  print(player)
 
 # Remove players using pop()
+roster.pop(24)
+roster.pop(19)
+roster.pop(15)
+roster.pop(10)
+roster.pop(9)
+roster.pop(2)
 
 # Add players using append()
+roster.append("Eduardo Camavinga")
+roster.append("David Alaba")
 
 
+# Print the new roster
 print("\n------")
 print("\nThe new Real Madrid roster:\n")
-# Print the new roster using a for loop
+for player in roster:
+  print(player)
